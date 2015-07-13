d72832ffcdec20728eab732c584be420b37c7b8b
Johannes Mikulasch <mikulasc@in.tum.de>
2015-07-13 22:35:13 +0200

    Added new scenes in create_dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 
 import numpy as np
+import yaml
+import os
 
 import cameras
 import context
@@ -9,49 +11,71 @@ import materials
 import meshes
 import scenes
 
+
+def save_yaml(filename, scene, camera, time, verbose=False):
+    with open(filename, 'w') as f:
+        yaml.dump({'scene name' : scene.name,
+                   'time' : time,
+                   'camera': {'rotation' : camera.rotation.tolist(),
+                              'position' : camera.position.tolist()}}, f)
+
+    if verbose:
+        print("Saved camera params as %s" % filename)
+
 if __name__ == '__main__':
 
-    c = context.Context(width=512, height=424, show_framerate=True)
-    c.print_opengl_info()
+    c = context.Context(width=512, height=424, show_framerate=False)
+
+    ## Setup camera movement
     camera = cameras.PerspectiveCamera2()
-    # Setup camera movement
     r = 2.0
     w = 0.0
     for t in range(270):
-        if t <= 90:
+        if t < 90:
+            # Move to the left
             w -= 0.5 * np.pi / 90.
         else:
+            # Move to the right
             w += 0.5 * np.pi / 90.
-        camera.set_frame(t, position=np.array([r * np.sin(w), 0, r * np.cos(w)]), rotation=np.array([0, w, 0]))
+        camera.set_frame(t, position=np.array([r * np.sin(w), 0, r * np.cos(w) -2]), rotation=np.array([0, w, 0]))
 
-    pointlight = lights.PointLight(position=np.array([0, 0, 0]), color=np.array([1, 1, 1, 1]), falloff=0)
-    sphere_geometry = meshes.IcosphereGeometry(subdivisions=4)
+
+    ## Setup Lights
+    ambientlight = lights.AmbientLight(color=np.array([0.2, 0.2, 0.2, 1]))
+    pointlight_origin = lights.PointLight(position=np.array([0, 0, 0]), color=np.array([1, 1, 1, 1]), falloff=0.5)
+    pointlight_right = lights.PointLight(position=np.array([1.5, 2, -2]), color=np.array([1, 1, 1, 1]), falloff=0.5)
+    ir_light = lights.PointLight(position=np.array([0, 0, 0]), color=np.array([1, 1, 1, 1]), falloff=0.0, name="IR Light")
 
     ## Setup Sphere Scene
+    sphere_geometry = meshes.IcosphereGeometry(subdivisions=4)
     scene_sphere = scenes.Scene(backgroundcolor=np.array([0., 0, 0, 1]), name="sphere")
-    sphere = meshes.Mesh(name='Square 1', position=np.array([0, 0, -3.5]), geometry=sphere_geometry,
+    sphere = meshes.Mesh(name='Square 1', position=np.array([0, 0, -2]), geometry=sphere_geometry,
                          material=materials.LambertianMaterial())
     scene_sphere.add(sphere)
-    scene_sphere.add(pointlight)
+    scene_sphere.add(pointlight_origin)
 
     ## Setup Head Scene
-
     scene_head = scenes.Scene(backgroundcolor=np.array([0, 0, 0, 1]), name="head")
     head_geometry = meshes.ObjectFile('./assets/humanface.obj')
     head_material = materials.BlinnPhongMaterial()
     head_material.add_colormap("./assets/humanface-texture.png")
-    head = meshes.Mesh(name='Face', position=np.array([0, 0, -1]), geometry=head_geometry, material=head_material)
+    head = meshes.Mesh(name='Face', position=np.array([0, 0, -2]), geometry=head_geometry, material=head_material)
     scene_head.add(head)
-    # scene_head.add(pointlight)
-    light = lights.PointLight(position=np.array([0, 2, 0]), color=np.array([1, 1, 1, 1]), falloff=0.5)
-    scene_head.add(light)
-    light = lights.PointLight(position=np.array([1.5, 2, -2]), color=np.array([1, 1, 1, 1]), falloff=0.5)
-    scene_head.add(light)
-    light = lights.AmbientLight(color=np.array([0.3, 0.3, 0.3, 1]))
-    scene_head.add(light)
+    scene_head.add(ambientlight)
+    scene_head.add(pointlight_origin)
+    # scene_head.add(pointlight_right)
 
     scenes = [scene_sphere, scene_head]
-    path = "images/dataset"
+    path = "~/dataset"
+    path = os.path.expanduser(path)
+
+    # Wait for confirmation
+    user_reply = raw_input("Data will be created in %s. Continue? [y/n]" % (path))
+    if user_reply != 'y' and user_reply != 'Y':
+        exit()
+
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     for scene in scenes:
 
@@ -61,16 +85,32 @@ if __name__ == '__main__':
         for t in range(270):
             camera.set_current_frame(t)
             c.render(scene, camera)
-            c.screenshot(scene, camera, "%s/%s_color.tiff" % (path, stripped_name))
+            save_yaml("%s/%s_%04d_camera.yaml" % (path, stripped_name, t), scene, camera, t, verbose=False)
+            c.screenshot(scene, camera, "%s/%s_%04d_color.tiff" % (path, stripped_name, t), verbose=False)
 
         # Normal
         for mesh in scene.meshes:
             mesh.material = materials.NormalMaterial()
-
-        c.screenshot(scene, camera, "%s/%s_normal.tiff" % (path, stripped_name))
+        for t in range(270):
+            camera.set_current_frame(t)
+            c.render(scene, camera)
+            c.screenshot(scene, camera, "%s/%s_%04d_normal.tiff" % (path, stripped_name, t), verbose=False)
 
         # Depth
         for mesh in scene.meshes:
             mesh.material = materials.DepthMaterial()
+        for t in range(270):
+            camera.set_current_frame(t)
+            c.render(scene, camera)
+            c.screenshot_bw(scene, camera, "%s/%s_%04d_depth.tiff" % (path, stripped_name, t), verbose=False)
 
-        c.screenshot_bw(scene, camera, "%s/%s_depth.tiff" % (path, stripped_name))
+        # IR
+        scene.remove_lights()
+        scene.add(ir_light)
+        for mesh in scene.meshes:
+            mesh.material = materials.LambertianMaterial()
+        for t in range(270):
+            camera.set_current_frame(t)
+            ir_light.set_position(camera.position)
+            c.render(scene, camera)
+            c.screenshot_bw(scene, camera, "%s/%s_%04d_ir.tiff" % (path, stripped_name, t), verbose=False)
