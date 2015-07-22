6d7107f82ac58b71d75a40b13b0f1aee59bf4a37
Johannes Mikulasch <mikulasc@in.tum.de>
2015-07-23 00:11:07 +0200

    simplified organisation of lights
    [position, 3] would get position

--- dataset-synth/lights.py
+++ dataset-synth/lights.py
@@ -20,17 +20,23 @@ class Light(object):
         self.number = 0
 
     def put_up_uniforms(self, shader):
-        for uniform_name in self.uniforms:
-            uniformvalue, number_of_values = self.uniforms[uniform_name]
+        for uniform_name, uniform_value in self.uniforms.items():
             location = glGetUniformLocation(shader.program, '%s[%i].%s' % (self.lighttype, self.number, uniform_name))
-            if number_of_values == 1:
-                glUniform1f(location, uniformvalue)
-            elif number_of_values == 2:
-                glUniform2f(location, *uniformvalue)
-            elif number_of_values == 3:
-                glUniform3f(location, *uniformvalue)
-            elif number_of_values == 4:
-                glUniform4f(location, *uniformvalue)
+            if type(uniform_value) is list:
+                uniform_value_len = len(uniform_value)
+            elif type(uniform_value) is np.ndarray:
+                uniform_value_len = uniform_value.size
+            elif type(uniform_value) is int or float:
+                uniform_value_len = 1
+
+            if uniform_value_len == 1:
+                glUniform1f(location, uniform_value)
+            elif uniform_value_len == 2:
+                glUniform2f(location, *uniform_value)
+            elif uniform_value_len == 3:
+                glUniform3f(location, *uniform_value)
+            elif uniform_value_len == 4:
+                glUniform4f(location, *uniform_value)
 
     def _register_name(self, name):
 
@@ -51,7 +57,7 @@ class Light(object):
             Light.registered_names.append(name)
 
     def set_position(self, position):
-        self.uniforms['position'] = [position, 3]
+        self.uniforms['position'] = position
 
     def __repr__(self):
         return self.__class__.__name__ + " (name: " + self.name + ")"
@@ -65,7 +71,7 @@ class DirectionalLight(Light):
         self.falloff = falloff
         self.direction = direction / np.linalg.norm(direction)
 
-        self.uniforms = {'color': [color, 4], 'direction': [direction, 3]}
+        self.uniforms = {'color': color, 'direction': direction}
 
         self.number = DirectionalLight.counter
         DirectionalLight.counter += 1
@@ -75,7 +81,7 @@ class PointLight(Light):
     def __init__(self, position, color, falloff, name='Pointlight'):
         self.lighttype = 'pointlights'
         self._register_name(name)
-        self.uniforms = {'color': [color, 4], 'position': [position, 3], 'falloff': [falloff, 1]}
+        self.uniforms = {'color': color, 'position': position, 'falloff': falloff}
 
         self.number = PointLight.counter
         PointLight.counter += 1
@@ -85,7 +91,7 @@ class AmbientLight(Light):
     def __init__(self, color, name='Ambientlight'):
         self.lighttype = 'ambientlights'
         self._register_name(name)
-        self.uniforms = {'color': [color, 4]}
+        self.uniforms = {'color': color}
 
         self.number = AmbientLight.counter
         AmbientLight.counter += 1
@@ -95,8 +101,8 @@ class SpotLight(Light):
     def __init__(self, position, direction, cone_angle, color, falloff, name='Spotlight', ):
         self.lighttype = 'spotlights'
         self._register_name(name)
-        self.uniforms = {'color': [color, 4], 'position': [position, 3], 'direction': [direction, 3],
-                         'cone_angle': [cone_angle, 1], 'falloff': [falloff, 1]}
+        self.uniforms = {'color': color, 'position': position, 'direction': direction,
+                         'cone_angle': cone_angle, 'falloff': falloff}
 
         self.number = SpotLight.counter
         SpotLight.counter += 1
