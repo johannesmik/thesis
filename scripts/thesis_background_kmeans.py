b1edd8a363095a670b9fc94f42bd0ffabb5c8fe5
Johannes Mikulasch <mikulasc@in.tum.de>
2015-08-16 04:19:35 +0200

    image to explain k-means

--- scripts/thesis_background_kmeans.py
+++ scripts/thesis_background_kmeans.py
@@ -1,49 +1,49 @@
-__author__ = 'johannes'
-
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
+from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import KMeans
 from PIL import Image
-
 
 depth_image = Image.open("../assets/testimages/sphere-depth-small.png")
 depth_image = np.asarray(depth_image, dtype=np.float32)
 
-shape_x, shape_y = depth_image.shape[:2]
-y, x = np.meshgrid(np.linspace(0, shape_y, shape_y), np.linspace(0, shape_x, shape_x))
+depth_cut = ((255 - depth_image[25,:]) - 76 )/ 4.
 
-X = np.vstack([x.flatten(), y.flatten(), depth_image.flatten()])
+print depth_cut
+x = np.linspace(1, 50, 50)
+
+X = np.vstack([x, depth_cut])
 X = X.T
 
-initial_clusters = np.array([X[25 * 50 + 15], X[25 * 50 + 35], X[15 * 50 + 25], X[35 * 50 + 25], X[25 * 50 + 25]])
+initial_clusters = np.array([X[15], X[25], X[35], X[40], X[45]])
 
-print initial_clusters
-print "X", X[25 * 50 + 25]
+for i in [0, 1, 2, 5, 20]:
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x, y, - depth_image, c='r', marker='.', alpha=0.25)
-ax.scatter(initial_clusters[:,0], initial_clusters[:,1], - initial_clusters[:,2] + 0.05, s=30, alpha=1)
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('- Z')
+    if i == 0:
+        nn = NearestNeighbors(1, 1)
+        nn.fit(initial_clusters)
+        centers = initial_clusters
+        dist, labels = nn.kneighbors(X)
+    else:
+        mbk = KMeans(max_iter=i, n_clusters=5,  init=initial_clusters, verbose=0, n_init=1)
+        mbk.fit(X)
+        centers = mbk.cluster_centers_
+        labels = mbk.labels_
 
-for i in [1, 2, 5, 20]:
 
-    mbk = KMeans(max_iter=i, n_clusters=5,  init=initial_clusters, verbose=0, n_init=1)
-    mbk.fit(X)
-
-    centers = mbk.cluster_centers_
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x, y, - depth_image, c='r', marker='.', alpha=0.25)
-    ax.scatter(centers[:,0], centers[:,1], - centers[:,2] + 0.05, s=30, alpha=1)
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('- Z')
-    plt.title('Iteration %d ' % i)
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=150, facecolor='w', edgecolor='k')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    plt.xlim([0, 51])
+    plt.ylim([-1, 50])
+    ax.set_xlabel('X', fontsize=22)
+    ax.set_ylabel('Z', fontsize=22)
+    plt.title('Iteration %d ' % i, fontsize=24)
+    ax.scatter(x, depth_cut, c=labels / 4., marker='o', alpha=1, s=90, edgecolors='none', cmap=plt.get_cmap("rainbow"))
+    ax.scatter(centers[:,0], centers[:,1], c=np.linspace(0, 1, 5), s=550, marker='*', alpha=1, cmap=plt.get_cmap("rainbow"))
 
 plt.show()
